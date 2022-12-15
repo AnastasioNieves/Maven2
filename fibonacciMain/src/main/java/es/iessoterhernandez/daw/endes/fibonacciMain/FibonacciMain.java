777f9f16ef3c77e42975ad524ea4b19eecc1ed37
Anastasio Nieves <82782955+AnastasioNieves@users.noreply.github.com>
2022-12-15 08:43:21 +0100

    Add files via upload

---
+++ fibonacciMain/src/main/java/es/iessoterhernandez/daw/endes/fibonacciMain/FibonacciMain.java
@@ -0,0 +1,18 @@
+package es.iessoterhernandez.daw.endes.fibonacciMain;
+import es.iessoterhernandez.daw.endes.fibonacci.*;
+/**
+ * Hello world!
+ *
+ */
+public class FibonacciMain 
+{
+    public static void main( String[] args )
+    {
+        
+    	Fibonacci f = new Fibonacci();
+    	
+    	f.fibonacci();
+    	
+    	
+    }
+}
